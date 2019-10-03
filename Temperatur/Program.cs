a75889c9af1a851586e208b51804a60deba4c012
Dimas Ananda <traperwaze@gmail.com>
2019-10-03 07:41:51 +0700

    SUCCESS FROM CELCIUS

--- Temperatur/Program.cs
+++ Temperatur/Program.cs
@@ -18,28 +18,18 @@ namespace konversiTemperatur {
             }
             
             //ULANGI
-            ulangan:
-            Console.Write("\n\nUlangi? (y/n) ");
-            char ulang = char.Parse(Console.ReadLine());
-            if (ulang=='y') {
-                goto main;
-            }
-            else if (ulang == 'n')
-            {
-                Environment.Exit(0);
-            }
-            else {
-                Console.WriteLine("INPUT SALAH!\n");
-                goto ulangan;
-            }
+            goto main;
         }
 
         //METHOD DARI C
         protected static void temp_c() {
+            main:
+            double c=0, f=0, k=0, r=0;
             pilihan:
             Console.WriteLine("UBAH KE :::");
-            Console.Write("1. R\t2. F\t3. K");
+            Console.Write("1. R\t2. F\t3. K ");
             int pilih_ke = int.Parse(Console.ReadLine());
+            Console.WriteLine();
             switch (pilih_ke) { 
                 case 1:
                     goto ke_r;
@@ -55,6 +45,48 @@ namespace konversiTemperatur {
                     break;
             }
 
+            //KE R
+            ke_r:
+            Console.Write("C = ");
+            c = double.Parse(Console.ReadLine());
+            r = c * 0.8;
+            Console.Write("R = " + System.Math.Round(r,2));
+            goto ulangan;
+
+            //KE F
+            ke_f:
+            Console.Write("C = ");
+            c = double.Parse(Console.ReadLine());
+            f = (c * 1.8)+32;
+            Console.Write("F = " + System.Math.Round(f, 2));
+            goto ulangan;
+
+            //KE K
+            ke_k:
+            Console.Write("C = ");
+            c = double.Parse(Console.ReadLine());
+            k = c + 273.15;
+            Console.Write("K = "+System.Math.Round(k,2));
+            goto ulangan;
+
+            //ULANGI
+            ulangan:
+            Console.Write("\n\nUlangi?\n1. Ya\t2. Dari Awal  ");
+            char ulang = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (ulang == '1')
+            {
+                goto main;
+            }
+            else if (ulang == '2')
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("INPUT SALAH!\n");
+                goto ulangan;
+            }
 
         }
     }
