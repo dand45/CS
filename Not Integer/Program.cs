cd7d7966d72dd3e2cc4d0a348e4e027442ea617a
Dimas Ananda <traperwaze@gmail.com>
2019-10-07 20:30:54 +0700

    update
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

---
+++ Not Integer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Not_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        main:
+            Console.Write("INPUT A NUMBER : ");
+            string num1 = Console.ReadLine();
+
+            try
+            {
+                int num2 = int.Parse(num1);
+            }
+            catch (FormatException) {
+                Console.WriteLine("\n{0}WRONG!\n");
+            }
+            Console.WriteLine(num1);
+            goto main;
+        }
+    }
+}
---
+++ WHILE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a < 10) {
+                Console.WriteLine(a);
+                a++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
